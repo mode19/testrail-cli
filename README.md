88ed17598eda2e96a7f594e90c74199fb9f67628
Eric Peterson <epeterson@tableausoftware.com>
2015-09-24 18:05:55 -0700

    Initial commit...

---
+++ README.md
@@ -0,0 +1,146 @@
+# TestRail CLI
+
+A simple command line tool to report test runs to TestRail from a build server.
+
+
+## Installation and usage
+
+To install testrail-cli, ensure you have node installed, then run
+`npm install -g testrail-cli`
+
+To run commands, change directories to the root of your working project (where
+you will likely have a `.testrail-cli.yml` file--see details below), and run a
+command like this:
+
+```sh
+testrail-cli init --runName="Name of my test run"
+```
+
+Full configuration and command details below.
+
+
+## Configuration
+
+This CLI provides configuration in two places: environment variables (for all
+configs related to TestRail authentication), and a `.testrail-cli.yml` file,
+assumed to be in the working directory from which the CLI is run. Details for
+each below.
+
+#### Environment Variables
+
+You must provide TestRail authentication information via environment variables.
+It's assumed that you are storing these securely.
+
+__`TESTRAIL_URL`__ - The URL of your TestRail instance.
+`export TESTRAIL_URL=https://[yourinstance].testrail.net`
+
+__`TESTRAIL_UN`__ - The username/e-mail address to authenticate with
+`export TESTRAIL_UN=user@example.com`
+
+__`TESTRAIL_PW`__ - The password associated with the aforementioned username.
+`export TESTRAIL_PW=yourPasswordhere`
+
+Note that the API must be enabled for your TestRail instance.
+
+#### `.testrail-cli.yml`
+
+It's recommended that you also supply additional configurations in a dot-file
+at the root of your project. This file (`.testrail-cli.yml`) must contain a map
+of test names to TestRail case IDs, but can also be used to provide default
+values for other useful properties. An example is provided below:
+
+```yaml
+# The ID of the default project against which to run commands.
+projectId: 2
+
+# The ID of the default test suite used for test run reporting.
+suiteId: 2
+
+# A mapping that relates testcase names to TestRail case IDs. The testcase name
+# (on the left) represents the "name" attribute on the "testcase" element in
+# your junit XML files.
+caseNameToIdMap:
+  "Name of Test Case 7": 7
+  "Another Test Name": 8
+  "Not to be confused with the class name": 14
+```
+
+
+## Commands
+
+A list of currently supported commands and their arguments/flags.
+
+#### `testrail-cli init`
+
+Used to initialize a test run in a given project. On success, the command will
+return a runId.
+
+__Arguments__
+- `--projectId` or `-p`
+  - Required if no ID is provided in `.testrail-cli.yml`. The value should
+    be the ID of the project for which you are beginning a test run.
+- `--runName` or `-n`
+  - Required. A name representing this test run.
+- `--suiteId` or `-s`
+  - Optional. The ID of the test suite to use when initializing the run. If
+    none is supplied, then a suite containing all cases is used. This can
+    also be supplied via `.testrail-cli.yml`.
+- `--description` or `-d`
+  - Optional. If desired, you can pass a description of this test run.
+- `--milestoneId` or `-m`
+  - Optional. If desired, you can pass the ID of a milestone to associate
+    with this test run.
+
+__Examples__
+Start a test run called "Automated Run" against project with ID 5.
+```sh
+testrail-cli init --projectId=5 --runName="Automated Run"
+```
+
+Start a test run called "Another Run" where the projectId is assumed from a
+`.testrail-cli.yml` file. Stash the runId for later commands.
+```sh
+export TESTRAIL_RUNID=`testrail-cli init -n "Another Run"
+```
+
+#### `testrail-cli report`
+
+Used to report test results for a given test run. On success, the command will
+report the number of case runs successfully reported.
+
+Note that this tool makes the assumption that you have created a map in your
+`.testrail-cli.yml` file that maps test case names (as reported in your junit
+XML) to test case IDs in TestRail. An example is highlighted in the
+"configuration" section above.
+
+__Arguments__
+- `--runId` or `-r`
+  - Required. The ID of the test run for which you wish to report test results.
+- `--file` or `-f`
+  - Required. The path to a junit XML file (or a path to a directory containing
+    such files).
+
+__Examples__
+Report a test run for a single XML file and known runId.
+```sh
+testrail-cli report --runId=5 --file=logs/junit.xml
+```
+
+Report a test run for a directory of junit XML files using a runId pulled from
+environment variables.
+
+testrail-cli report -r $TESTRAIL_RUNID -f /path/to/junit/
+
+#### `testrail-cli finish`
+
+Used to close a given test run.
+
+__Arguments__
+- `--runId` or `-r`
+  - Required. The ID of the test run you wish to close.
+
+__Examples__
+Close a test run based on an ID in an environment variable.
+```sh
+testrail-cli finish --runId=$TESTRAIL_RUNID
+```
---
+++ src/core.js
@@ -0,0 +1,207 @@
+"use strict";
+
+var Promise = require('promise'),
+    fs = require('fs'),
+    readFile = Promise.denodeify(fs.readFile),
+    XmlParser = require('xml-parser'),
+    HtmlEntitiesFactory = require('html-entities').AllHtmlEntities,
+    HtmlEntities = new HtmlEntitiesFactory();
+
+/**
+ * Instantiates a "core" object with given dependencies. The object consists of
+ * properties that represent methods to be run on corresponding commands.
+ *
+ * @param TestRail
+ * @param {object} configs
+ * @param process
+ * @param console
+ * @returns {{init: Function, finish: Function, report: Function}}
+ */
+module.exports = function constructCore(TestRail, configs, process, console) {
+  process = process || global.process;
+  console = console || global.console;
+
+  var apiCallsAttempted = 0,
+      maxCallAttemptsAllowed = 5;
+
+  return {
+    /**
+     * Initializes/adds a run to TestRail for the given project ID.
+     *
+     * @param {int} projectId
+     *   Required. The project ID for which a new run should be added.
+     * @param {string} name
+     *   Required. The name of the run to be added.
+     * @param {int} suiteId
+     *   Optional. The ID of the test suite to be run.
+     * @param {string} description
+     *   Optional. A description to go along with the test run.
+     * @param {int} milestoneId
+     *   Optional. The ID of a milestone with which to associate this run.
+     */
+    init: function initializeTestRun(projectId, name, suiteId, description, milestoneId) {
+      if (!projectId || !name) {
+        console.error('You must supply a projectId (-p or --projectId=) and runName (-n or --runName=).');
+        process.exit(1);
+      }
+
+      TestRail.addRun(projectId, suiteId, name, description, milestoneId, function (response) {
+        response = typeof response === 'string' ? JSON.parse(response) : response;
+        if (response.id) {
+          console.log(response.id);
+          process.exit(0);
+        }
+        else {
+          // Retry if we're under the limit.
+          if (apiCallsAttempted < maxCallAttemptsAllowed) {
+            apiCallsAttempted++;
+            initializeTestRun();
+          }
+          else {
+            console.error('Error initializing test run in TestRail.');
+            console.error(response);
+            process.exit(1);
+          }
+        }
+      });
+    },
+
+    /**
+     * Marks a given test run as closed on TestRail.
+     *
+     * @param {int} runId
+     *   Required. The ID of the test run to close.
+     */
+    finish: function closeTestRun(runId) {
+      if (!runId) {
+        console.error('You must supply a runId (-r or --runId=).');
+        process.exit(1);
+      }
+
+      TestRail.closeRun(runId, function (response) {
+        response = typeof response === 'string' ? JSON.parse(response) : response;
+        if (response.completed_on) {
+          console.log('Successfully closed test run ' + runId + '.');
+          process.exit(0);
+        }
+        else {
+          if (apiCallsAttempted < maxCallAttemptsAllowed) {
+            apiCallsAttempted++;
+            closeTestRun();
+          }
+          else {
+            console.error('There was an error closing the test run.');
+            console.error(response);
+            process.exit(1);
+          }
+        }
+        console.log(response);
+      });
+    },
+
+    /**
+     * Given a junit XML file (or a directory of files), processes all test
+     * results, maps them to cases, and pushes the results to TestRail.
+     *
+     * @param {int} runId
+     *   The ID of the run with which to associate the cases.
+     * @param {string} fileOrDir
+     *   The path to the junit XML file or directory of files.
+     */
+    report: function reportXml(runId, fileOrDir) {
+      var files = [],
+          caseResults = [],
+          fsStat;
+
+      if (!fileOrDir || !runId) {
+        console.error('You must supply a file (-f or --file=) and runId (-r or --runId=).');
+        process.exit(1);
+      }
+
+      // Stat the file.
+      fsStat = fs.statSync(fileOrDir);
+
+      if (fsStat.isFile()) {
+        // Make sure the provided file is an XML file.
+        if (fileOrDir.substring(fileOrDir.length - 4) === '.xml') {
+          files.push(fileOrDir);
+        }
+      }
+      else if (fsStat.isDirectory()) {
+        // Filter down to just those files that are XML.
+        files = fs.readdirSync(fileOrDir).filter(function(dirContent) {
+          return dirContent.substring(dirContent.length - 4) === '.xml';
+        }).map(function (dirContent) {
+          return fileOrDir + (fileOrDir.substring(fileOrDir.length - 1) === '/' ? '' : '/') + dirContent
+        });
+      }
+
+      // Asynchronously read in all files in the file array.
+      Promise.all(files.map(function readFilesPromises(file) {
+        return readFile(file, 'utf8');
+      })).done(function (fileContents) {
+        fileContents.forEach(function (rawXml) {
+          var xml = XmlParser(rawXml);
+
+          if (!xml.root.name || xml.root.name !== 'testsuite') {
+            console.error('Invalid xml. Expected root name testsuite');
+            process.exit(1);
+          }
+
+          if (xml.root && xml.root.children && xml.root.children.length) {
+            xml.root.children.forEach(function (testcase) {
+              var caseResult = {};
+
+              if (testcase.name && testcase.name === 'testcase') {
+                // Universal to pass or fail.
+                caseResult.case_id = configs.caseNameToIdMap[HtmlEntities.decode(testcase.attributes.name)];
+                caseResult.elapsed = Math.ceil(testcase.attributes.time) + 's';
+                caseResult.version = 'test';
+
+                // If testcase.children is empty, the test case passed. 1 means pass.
+                if (testcase.children.length === 0) {
+                  caseResult.status_id = 1;
+                }
+                // Otherwise, there was a failure. 5 means failure. Add fail message.
+                else {
+                  caseResult.status_id = 5;
+                  caseResult.comment = HtmlEntities.decode(testcase.children[0].attributes.message);
+                }
+
+                caseResults.push(caseResult);
+              }
+            });
+          }
+        });
+
+        // Post results if we had any.
+        if (caseResults.length) {
+          (function addResultsForCasesAttempt() {
+            TestRail.addResultsForCases(runId, {results: caseResults}, function (response) {
+              response = typeof response === 'string' ? JSON.parse(response) : response;
+
+              if (response instanceof Array && response.length) {
+                console.log('Successfully uploaded ' + response.length + ' test case results to TestRail.');
+                process.exit(0);
+              }
+              else {
+                if (apiCallsAttempted < maxCallAttemptsAllowed) {
+                  apiCallsAttempted++;
+                  addResultsForCasesAttempt();
+                }
+                else {
+                  console.error('There was an error uploading test results to TestRail.');
+                  console.error(response);
+                  process.exit(1);
+                }
+              }
+            });
+          })();
+        }
+        else {
+          console.log('Did not parse any test XML files.');
+        }
+      });
+    }
+  };
+};
